d41d828f2b270b6cde7d5194e170e635876b9956
Paolo Montesel <git@babush.me>
2024-05-30 15:54:32 +0900

    implot: bring back missing aliases and comments

--- src/lib.rs
+++ src/lib.rs
@@ -12,8 +12,25 @@ pub struct PlotUi<'ui> {
     context: &'ui Context,
 }
 
+/// Choice of axis.
 pub type AxisChoice = sys::ImAxis_;
+
+/// Markers.
 pub type Marker = sys::ImPlotMarker_;
+
+/// Colorable plot elements. These are called "ImPlotCol" in ImPlot itself, but I found that
+/// name somewhat confusing because we are not referring to colors, but _which_ thing can
+/// be colored - hence I added the "Element".
 pub type PlotColorElement = sys::ImPlotCol_;
+
+/// Colormap choice.
+pub type Colormap = sys::ImPlotColormap_;
+
+/// Style variable choice, as in "which thing will be affected by a style setting".
+pub type StyleVar = sys::ImPlotStyleVar_;
+
+/// Used to position items on a plot (e.g. legends, labels, etc.)
 pub type PlotLocation = sys::ImPlotLocation_;
+
+/// Used to hide/show legends, shoe them horizontally, etc.
 pub type PlotLegendFlags = sys::ImPlotLegendFlags_;
